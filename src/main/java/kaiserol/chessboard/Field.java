c6a4916671d46bdfeeaed88f01b937e39a47bf1b
kaiserol <oliver10.kaiser@gmail.com>
2026-01-06 14:51:01 +0100

    Setup Field

--- src/main/java/kaiserol/chessboard/Field.java
+++ src/main/java/kaiserol/chessboard/Field.java
@@ -1,4 +1,47 @@
 package kaiserol.chessboard;
 
+import kaiserol.pieces.Piece;
+
 public class Field {
+    private final int x;
+    private final int y;
+    private Piece piece;
+
+    public Field(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isEmpty() {
+        return piece == null;
+    }
+
+    public Piece get() {
+        return piece;
+    }
+
+    public Piece put(Piece piece) {
+        Piece oldPiece = this.piece;
+        this.piece = piece;
+        return oldPiece;
+    }
+
+    public Piece remove() {
+        return put(null);
+    }
+
+    @Override
+    public String toString() {
+        char coord1 = (char) ('a' + (x - 1));
+        char coord2 = (char) ('1' + (y - 1));
+        return coord1 + "" + coord2;
+    }
 }
