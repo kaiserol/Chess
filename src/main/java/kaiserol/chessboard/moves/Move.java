fcd718db2115fee811405296b8602d7d684ec15b
kaiserol <oliver10.kaiser@gmail.com>
2026-01-06 17:11:17 +0100

    PawnExchange renamed to PawnPromotion and implemented it

--- src/main/java/kaiserol/chessboard/moves/Move.java
+++ src/main/java/kaiserol/chessboard/moves/Move.java
@@ -1,6 +1,6 @@
 package kaiserol.chessboard.moves;
 
-public abstract sealed class Move permits NormalMove, Castling, EnPassant, PawnJump, PawnExchange {
+public abstract sealed class Move permits NormalMove, Castling, EnPassant, PawnJump, PawnPromotion {
     public abstract void execute();
 
     public abstract void undo();
---
+++ src/main/java/kaiserol/chessboard/moves/PawnPromotion.java
@@ -0,0 +1,59 @@
+package kaiserol.chessboard.moves;
+
+import kaiserol.chessboard.Field;
+import kaiserol.pieces.Pawn;
+import kaiserol.pieces.Piece;
+
+public final class PawnPromotion extends Move {
+    private final Field start;
+    private final Field target;
+    private final Pawn pawn;
+    private final Piece promotedPiece;
+    private final Piece capturedPiece;
+
+    public PawnPromotion(Pawn pawn, Field target, Piece promotedPiece) {
+        this.start = pawn.getField();
+        this.target = target;
+
+        this.pawn = pawn;
+        this.promotedPiece = promotedPiece;
+        this.capturedPiece = target.get();
+    }
+
+    @Override
+    public void execute() {
+        // Moves the pawn and promotes it
+        start.remove();
+        target.put(promotedPiece);
+
+        pawn.setField(null);
+        promotedPiece.setField(target);
+
+        // Removes the piece to capture
+        if (capturedPiece != null) {
+            capturedPiece.setField(null);
+        }
+
+        // Increases the moves
+        pawn.increaseMoveCount();
+    }
+
+    @Override
+    public void undo() {
+        // Puts the piece to capture back
+        if (capturedPiece != null) {
+            target.put(capturedPiece);
+            capturedPiece.setField(target);
+        } else {
+            target.remove();
+        }
+
+        // Moves the pawn back and removes the promoted piece
+        start.put(pawn);
+        pawn.setField(start);
+        promotedPiece.setField(null);
+
+        // Decreases the moves
+        pawn.decreaseMoveCount();
+    }
+}
