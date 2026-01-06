1b61315db6f35a2b3c9269b91ea233aca345a502
kaiserol <oliver10.kaiser@gmail.com>
2026-01-06 17:02:48 +0100

    PawnJump added

--- src/main/java/kaiserol/chessboard/moves/Move.java
+++ src/main/java/kaiserol/chessboard/moves/Move.java
@@ -1,6 +1,6 @@
 package kaiserol.chessboard.moves;
 
-public abstract sealed class Move permits NormalMove, CastlingMove, EnPassantMove, PawnExchange {
+public abstract sealed class Move permits NormalMove, CastlingMove, EnPassantMove, PawnJump, PawnExchange {
     public abstract void execute();
 
     public abstract void undo();
