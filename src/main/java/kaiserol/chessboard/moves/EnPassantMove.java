144e2c0400a71cdb730b0eaa8b0bb77b81b03bd0
kaiserol <oliver10.kaiser@gmail.com>
2026-01-06 16:35:16 +0100

    EnpassantMove implemented

--- src/main/java/kaiserol/chessboard/moves/EnPassantMove.java
+++ src/main/java/kaiserol/chessboard/moves/EnPassantMove.java
@@ -1,13 +1,52 @@
 package kaiserol.chessboard.moves;
 
+import kaiserol.chessboard.Field;
+import kaiserol.pieces.Pawn;
+
 public final class EnPassantMove extends Move {
+    private final Pawn pawn;
+    private final Field pawnStart;
+    private final Field pawnTarget;
+
+    private final Pawn pawnToCapture;
+    private final Field pawnToCaptureField;
+
+    public EnPassantMove(Pawn pawn, Field pawnTarget, Pawn pawnToCapture) {
+        this.pawn = pawn;
+        this.pawnStart = pawn.getField();
+        this.pawnTarget = pawnTarget;
+
+        this.pawnToCapture = pawnToCapture;
+        this.pawnToCaptureField = pawnToCapture.getField();
+    }
+
     @Override
     public void execute() {
+        // Moves the pawn
+        pawnStart.remove();
+        pawnTarget.put(pawn);
+        pawn.setField(pawnTarget);
 
+        // Removes the pawn to capture
+        pawnToCaptureField.remove();
+        pawnToCapture.setField(null);
+
+        // Increases the moves
+        pawn.increaseMoveCount();
     }
 
     @Override
     public void undo() {
+        // Moves the pawn back
+        pawnTarget.remove();
+        pawnStart.put(pawn);
+        pawn.setField(pawnStart);
 
+        // Puts the pawn to capture back
+        pawnToCaptureField.put(pawnToCapture);
+        pawnToCapture.setField(pawnToCaptureField);
+
+        // Decreases the moves
+        pawn.decreaseMoveCount();
     }
 }
