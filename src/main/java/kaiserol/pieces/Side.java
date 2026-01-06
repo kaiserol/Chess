8c2a7521e4f02e9e4a07b215d6e466071f76f4d9
kaiserol <oliver10.kaiser@gmail.com>
2026-01-06 15:42:16 +0100

    Side update

--- src/main/java/kaiserol/pieces/Side.java
+++ src/main/java/kaiserol/pieces/Side.java
@@ -3,15 +3,15 @@ package kaiserol.pieces;
 public enum Side {
     WHITE, BLACK;
 
-    public Side opposite() {
-        return (this == WHITE) ? BLACK : WHITE;
-    }
-
     public boolean isWhite() {
         return this == WHITE;
     }
 
     public boolean isBlack() {
         return this == BLACK;
+    }
+
+    public Side opposite() {
+        return isWhite() ? BLACK : WHITE;
     }
 }
