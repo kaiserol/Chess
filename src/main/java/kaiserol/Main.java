792493fd590537f4f4b1d9473bdb6c81ce45ad76
kaiserol <oliver10.kaiser@gmail.com>
2026-01-09 12:37:37 +0100

    Implemented Chess simulation - Pre-defined moves are passed on - Added simulation command to the main method (parameters from the main method are checked)

--- src/main/java/kaiserol/Main.java
+++ src/main/java/kaiserol/Main.java
@@ -1,29 +1,21 @@
 package kaiserol;
 
 import kaiserol.handler.Game;
+import kaiserol.logic.ChessSimulator;
 
 public class Main {
     public static void main(String[] args) {
-        Game game = new Game();
+        if (args.length > 0 && args[0].equalsIgnoreCase("simulation")) {
+            ChessSimulator simulator = new ChessSimulator();
 
-        // Start game
-        System.out.println("Welcome to the game of chess! (Write 'exit' to exit)");
-        game.printBoard();
+            // Pawn Promotion
+            String[] whiteMoves = {"a2a4", "a4a5", "a5a6", "a6b7", "b7a8"};
+            String[] blackMoves = {"h7h6", "h6h5", "h5h4", "h4h3", "h3g2"};
 
-        String[] whiteMoves = {"a2a4", "a4a5", "a5a6", "a6b7", "b7a8"};
-        String[] blackMoves = {"h7h6", "h6h5", "h5h4", "h4h3", "h3g2"};
-
-        for (int i = 0; i < whiteMoves.length; i++) {
-            System.out.printf("Enter move (White's turn): %s%n", whiteMoves[i]);
-            game.executeMove(whiteMoves[i]);
-            game.printBoard();
-
-            System.out.printf("Enter move (Black's turn): %s%n", blackMoves[i]);
-            game.executeMove(blackMoves[i]);
-            game.printBoard();
+            simulator.runSimulation(whiteMoves, blackMoves);
+        } else {
+            Game game = new Game();
+            game.startGameLoop();
         }
-
-        // Start the game loop
-        game.startGameLoop();
     }
 }
