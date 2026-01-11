1876f8d9ffc7a3c6bd8bd3ec07780439f1fcaffc
kaiserol <oliver10.kaiser@gmail.com>
2026-01-11 18:25:12 +0100

    Extended TerminalChessSimulator to support multiple simulations and inherits ChessController functionality.

--- src/main/java/kaiserol/Main.java
+++ src/main/java/kaiserol/Main.java
@@ -10,13 +10,15 @@ public class Main {
         Game game = new Game();
 
         if (args.length > 0 && args[0].equalsIgnoreCase("-simulation")) {
-            TerminalChessSimulator simulator = new TerminalChessSimulator();
+            TerminalChessSimulator simulator = new TerminalChessSimulator(game);
 
             // Pawn Promotion
-            String[] whiteMoves = {"a2a4", "a4a5", "a5a6", "a6b7", "b7a8"};
+            String[] whiteMoves = {"a2a4", "a4a5", "a5a6", "a6b7", "b7a8", "d", "d"};
             String[] blackMoves = {"h7h6", "h6h5", "h5h4", "h4h3", "h3g2"};
+            simulator.addSimulation(whiteMoves, blackMoves);
 
-            simulator.runSimulation(whiteMoves, blackMoves);
+            // Runs the simulator
+            simulator.run();
         } else if (args.length > 0 && args[0].equalsIgnoreCase("-terminal")) {
             new TerminalChess(game).run();
         } else {
--- src/main/java/kaiserol/controller/TerminalChessSimulator.java
+++ src/main/java/kaiserol/controller/TerminalChessSimulator.java
@@ -1,12 +1,38 @@
 package kaiserol.controller;
 
-// TODO: Should also extend ChessController
-public class TerminalChessSimulator {
-    public void runSimulation(String[] whiteMoves, String[] blackMoves) {
-        Game game = new Game();
+import java.util.ArrayList;
+import java.util.List;
 
+public class TerminalChessSimulator extends ChessController {
+    private final List<Simulation> simulations;
+
+    public TerminalChessSimulator(Game game) {
+        super(game);
+        this.simulations = new ArrayList<>();
+    }
+
+    public void addSimulation(String[] whiteMoves, String[] blackMoves) {
+        if (whiteMoves == null || blackMoves == null) throwException("Moves cannot be null");
+        if (whiteMoves.length == 0 || blackMoves.length == 0) throwException("Moves cannot be empty");
+
+        int difference = whiteMoves.length - blackMoves.length;
+        if (difference < 0) throwException("White moves must be longer than black moves");
+        if (difference > 1) throwException("White moves must not be longer than black moves by more than 1 move");
+
+        simulations.add(new Simulation(whiteMoves, blackMoves));
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < simulations.size(); i++) {
+            Simulation simulation = simulations.get(i);
+            runSimulation(simulation.whiteMoves(), simulation.blackMoves(), i + 1);
+        }
+    }
+
+    private void runSimulation(String[] whiteMoves, String[] blackMoves, int simulationNumber) {
         String SEP = "-".repeat(8);
-        printMessage(SEP + " Simulation started " + SEP);
+        printMessage(SEP + simulationNumber + ". Simulation started " + SEP);
         printMessage("Initial state of the game:");
         game.getBoard().printBoard();
 
@@ -35,14 +61,21 @@ public class TerminalChessSimulator {
             }
         }
 
-        printMessage(SEP + " Simulation ended " + SEP);
+        printMessage(SEP + simulationNumber + ". Simulation ended " + SEP);
     }
 
-    private void printMessage(String msg) {
-        System.out.println(msg);
+    private void printMessage(String message) {
+        System.out.println(message);
     }
 
-    private void printError(String errorMsg) {
-        System.out.println("Error: " + errorMsg);
+    private void printError(String error) {
+        System.out.println("Error: " + error);
+    }
+
+    private void throwException(String message) {
+        throw new IllegalArgumentException(message);
+    }
+
+    private record Simulation(String[] whiteMoves, String[] blackMoves) {
     }
 }
